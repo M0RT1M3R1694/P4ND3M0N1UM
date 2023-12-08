ae6c8b83628804b9a305c1de3df3d2f73e4fe22d
M0RT1M3R1694 <128715892+M0RT1M3R1694@users.noreply.github.com>
2023-12-08 20:52:20 +0000

    update categories

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User,Book,Favorites,Category
+from .models import db, User,Book,Favorites,Categories
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -14,7 +14,7 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Book, db.session))
     admin.add_view(ModelView(Favorites, db.session))
-    admin.add_view(ModelView(Category, db.session))
+    admin.add_view(ModelView(Categories, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -7,7 +7,6 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     first_name = db.Column(db.String(80), nullable=False)
